9bca8b58cd78fc1b407ef76e4ca3003ce29b0d7c
shiguangpeng <shiguangpeng@ucen.cc>
2023-10-16 17:12:57 +0800

    :sparkles: 完成CSM算法编写，但是还没使用OOP的方法进行封装。

--- examples/Civic/main.cpp
+++ examples/Civic/main.cpp
@@ -1,142 +1,10 @@
-//#define _USE_MATH_DEFINES
-//
-//#include <cmath>
-//
-//// 系统方程和量测方程
-//#include "SystemModel.hpp"
-//#include "PositionMeasurementModel.hpp"
-//#include "VelocityMeasurementModel.hpp"
-//#include "AccMeasurementModel.hpp"
-//
-//#include <kalman/ExtendedKalmanFilter.hpp>
-//
-//#include <iostream>
-//#include <random>
-//#include <chrono>
-//
-//using namespace KalmanProject;
-//
-//typedef float T;
-//
-//// Some type shortcuts
-//typedef Civic::State<T> State;
-//typedef Civic::Control<T> Control;
-//typedef Civic::SystemModel<T> SystemModel;
-//
-//typedef Civic::PositionMeasurement<T> PositionMeasurement;
-//typedef Civic::PositionMeasurementModel<T> PositionModel;
-//typedef Civic::OrientationMeasurement<T> OrientationMeasurement;
-//
-//int main(int argc, char **argv) {
-//    // Simulated (true) system state
-//    State x;
-//    x.setZero();
-//
-//    // Control input
-//    Control u;
-//    // System
-//    SystemModel sys;
-//
-//    // Measurement models
-//    // Set position landmarks at (-10, -10) and (30, 75)
-//    PositionModel pm(-10, -10, 30, 75);
-//    OrientationModel om;
-//
-//    // Random number generation (for noise simulation)
-//    std::default_random_engine generator;
-//    generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
-//    std::normal_distribution<T> noise(0, 1);
-//
-//    // Some filters for estimation
-//    // Pure predictor without measurement updates
-//    Kalman::ExtendedKalmanFilter<State> predictor;
-//    // Extended Kalman Filter
-//    Kalman::ExtendedKalmanFilter<State> ekf;
-//    // Unscented Kalman Filter
-//    Kalman::UnscentedKalmanFilter<State> ukf(1);
-//
-//    // Init filters with true system state
-//    predictor.init(x);
-//    ekf.init(x);
-//    ukf.init(x);
-//
-//    // Standard-Deviation of noise added to all state vector components during state transition
-//    T systemNoise = 0.1;
-//    // Standard-Deviation of noise added to all measurement vector components in orientation measurements
-//    T orientationNoise = 0.025;
-//    // Standard-Deviation of noise added to all measurement vector components in distance measurements
-//    T distanceNoise = 0.25;
-//
-//    // Simulate for 100 steps
-//    const size_t N = 100;
-//    for (size_t i = 1; i <= N; i++) {
-//        // Generate some control input
-//        u.v() = 1. + std::sin(T(2) * T(M_PI) / T(N));
-//        u.dtheta() = std::sin(T(2) * T(M_PI) / T(N)) * (1 - 2 * (i > 50));
-//
-//        // Simulate system
-//        x = sys.f(x, u);
-//
-//        // Add noise: Our robot move is affected by noise (due to actuator failures)
-//        x.x() += systemNoise * noise(generator);
-//        x.y() += systemNoise * noise(generator);
-//        x.theta() += systemNoise * noise(generator);
-//
-//        // Predict state for current time-step using the filters
-//        auto x_pred = predictor.predict(sys, u);
-//        auto x_ekf = ekf.predict(sys, u);
-//        auto x_ukf = ukf.predict(sys, u);
-//
-//        // Orientation measurement
-//        {
-//            // We can measure the orientation every 5th step
-//            OrientationMeasurement orientation = om.h(x);
-//
-//            // Measurement is affected by noise as well
-//            orientation.theta() += orientationNoise * noise(generator);
-//
-//            // Update EKF
-//            x_ekf = ekf.update(om, orientation);
-//
-//            // Update UKF
-//            x_ukf = ukf.update(om, orientation);
-//        }
-//
-//        // Position measurement
-//        {
-//            // We can measure the position every 10th step
-//            PositionMeasurement position = pm.h(x);
-//
-//            // Measurement is affected by noise as well
-//            position.d1() += distanceNoise * noise(generator);
-//            position.d2() += distanceNoise * noise(generator);
-//
-//            // Update EKF
-//            x_ekf = ekf.update(pm, position);
-//
-//            // Update UKF
-//            x_ukf = ukf.update(pm, position);
-//        }
-//
-//        // Print to stdout as csv format
-//        std::cout << x.x() << "," << x.y() << "," << x.theta() << ","
-//                  << x_pred.x() << "," << x_pred.y() << "," << x_pred.theta() << ","
-//                  << x_ekf.x() << "," << x_ekf.y() << "," << x_ekf.theta() << ","
-//                  << x_ukf.x() << "," << x_ukf.y() << "," << x_ukf.theta()
-//                  << std::endl;
-//    }
-//
-//    return 0;
-//}
-
-
 /**
 * Simple Test program
 */
 
 #define _USE_MATH_DEFINES
 
-#include "Eigen/Core"
+#include "Eigen/Dense"
 #include <cmath>
 #include <iostream>
 
@@ -144,20 +12,28 @@
 
 int main(int argc, char *argv[]) {
     // 时间步长
-    const int T = 1;
+    const int T = 2;
     // 机动频率
     const float alpha = 0.2;
     // 加速度
-    const float acc = 1;
+    const float acc = 0.32*G;
+
+    // 测量值 y, y是4行1列的矩阵
+    Eigen::Matrix<float, 4, 1> yk;
+    // todo: input
+    yk << 30, 2.4, 43, 2.9;
 
     // 状态转移矩阵
-    Eigen::Matrix3f F;
-    F << 1, T, (1 / alpha * alpha) * (-1 + alpha * T + expf(-alpha * T)),
-            0, 1, (1 / alpha) * (1 - expf(-alpha * T)),
-            0, 0, expf(-alpha * T);
+    Eigen::Matrix<float, 6, 6> F;
+    F << 1, T, (1 / alpha * alpha) * (-1 + alpha * T + expf(-alpha * T)), 0, 0, 0,
+            0, 1, (1 / alpha) * (1 - expf(-alpha * T)), 0, 0, 0,
+            0, 0, expf(-alpha * T), 0, 0, 0,
+            0, 0, 0, 1, T, (1 / alpha * alpha) * (-1 + alpha * T + expf(-alpha * T)),
+            0, 0, 0, 0, 1, (1 / alpha) * (1 - expf(-alpha * T)),
+            0, 0, 0, 0, 0, expf(-alpha * T);
 
     // 控制矩阵U
-    Eigen::Matrix<float, 3, 2> U;
+    Eigen::Matrix<float, 6, 1> U;
     U << (1 / acc) * (-T + (acc * T * T) / 2 + (1 - expf(-acc * T) / acc)),
             T - (1 - expf(-acc * T) / acc),
             1 - expf((-acc * T)),
@@ -166,7 +42,7 @@ int main(int argc, char *argv[]) {
             1 - expf((-acc * T));
 
     // 状态方程噪声的方差
-    float max_acc = 0.5 * G;
+    float max_acc = 2 * G;
     float avg_acc = 0.2 * G;
 
     Eigen::Matrix3f Q;
@@ -200,24 +76,36 @@ int main(int argc, char *argv[]) {
     /** 卡尔曼滤波步骤 */
 
     /****************** 预测/估计 ******************/
-    Eigen::Matrix<float, 3, 2> x_hat;
+    Eigen::Matrix<float, 6, 1> x_hat;
     x_hat << 5, 1.2, 0.9, 7, 1.6, 0.7;
     // 1. 状态估计
-    Eigen::Matrix<float, 3, 2> x_hat_ = F * x_hat + (U * avg_acc);
-    std::cout << x_hat_ << std::endl;
+    Eigen::Matrix<float, 6, 1> x_hat_ = F * x_hat + (U * avg_acc);
 
     // 2. 计算协方差
-    Eigen::Matrix<float, 3, 3> pk;
-    pk << 0.2, 0.6, 0.9, 0.9, 0.25, 0.43, 0.9, 0.25, 0.3;
-    Eigen::Matrix<float, 3, 3> p_k = F * pk * F.transpose();
+    Eigen::Matrix<float, 6, 6> temp = Eigen::Matrix<float, 6, 6>::Random(6, 6);
+    // 模拟生成协方差矩阵
+    Eigen::Matrix<float, 6, 6> pk = temp * temp.transpose();
+    Eigen::Matrix<float, 6, 6> pk_ = F * pk * F.transpose();
 
     /***************** 更新/校正 ******************/
-    // 3. 卡尔曼增益K
-    Eigen::Matrix<float, 2, 3> H;// H为观测矩阵，因为只有位置观测值，因此只有两个
-    H << 1, 0, 0, 1, 0, 0;
-    Eigen::Matrix<float, 3, 3> Kk = (p_k * H.transpose()) / (H * p_k * H.transpose());
+    // 3. 卡尔曼增益K,
+    Eigen::Matrix<float, 4, 6> H;   // H是观测矩阵，这里观测了4个状态，分别是横纵向位置和速度
+    H << 1, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 0,
+            0, 0, 0, 0, 1, 0;
 
-    // 4. 后验估计
+    Eigen::Matrix<float, 6, 4> Kk = (pk_ * H.transpose()) * (H * pk_ * H.transpose()).inverse();
+
+    // 4. 观测值的后验估计
+    x_hat = x_hat_ + Kk * (yk - H * x_hat_);
+
+    // 5. 后验概率
+    Eigen::MatrixXf I;
+    pk = (I.setIdentity(6, 6) - Kk * H) * pk_;
+
+    std::cout << "x_hat=\n" << x_hat << "\n"
+              << "pk=\n" << pk << std::endl;
 
     return 0;
 }
