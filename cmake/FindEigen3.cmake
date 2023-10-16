231e258bbc997945f57c046c6c9a7ba1842630bf
shiguangpeng <742508176@qq.com>
2023-10-16 22:05:16 +0800

    :sparkles: 新增linux分支

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -2,10 +2,8 @@
 #  Eigen3_FOUND
 
 find_path( Eigen3_INCLUDE_DIR Eigen/Core
-#    /usr/include/eigen3
-#    /usr/local/include/eigen3
-    "C:/Program Files (x86)/Eigen3/include/eigen3"
-    "C:/Program Files (x86)/Eigen3/include/eigen3/unsupported"
+    /usr/include/eigen3
+    /usr/local/include/eigen3
 )
 
 
